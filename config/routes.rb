cca46375741a06d8945424e99af3106e33284ae4
GLORIA TUMUSHABE <gloriatumushabe@berkeley.edu>
2019-05-01 16:50:27 -0700

    Did find students

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get  '/welcome/tutor' => 'welcome#tutor', as: :welcome_tutor
   post 'tutees/login/' => 'tutees#login', as: :login_tutee
   get 'requests/history/:tutee_id' => 'requests#history', as: :request_history_tutee
-  get '/tutees/all/' => 'tutees#all', as: :tutee_all
+  get '/tutors/:tutor_id/find_students' => 'tutors#find_students', as: :tutor_find_students
 
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
